9404a8d22f7c8a7de4db22463e7c5e7d04ac74ac
sangeeta2003 <mishrasangeeta944@gmail.com>
2026-09-16 09:36:26 +0530

    learn streaming

--- streaming/streaming/src/main/java/coderarmy/in/streaming/ChatController.java
+++ streaming/streaming/src/main/java/coderarmy/in/streaming/ChatController.java
@@ -1,4 +1,27 @@
 package coderarmy.in.streaming;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
+
 public class ChatController {
+    private final ChatService chatService;
+    public ChatController(ChatService chatService){
+        this.chatService = chatService;
+    }
+    @PostMapping("/chat")
+    public Flux<String> chat(@RequestBody String message){
+        return chatService.chat(message);
+
+    }
+//    @DeleteMapping
+//    public void clearChat(){
+//        chatService.clearHistory();
+//    }
 }
--- streaming/streaming/src/main/java/coderarmy/in/streaming/ChatService.java
+++ streaming/streaming/src/main/java/coderarmy/in/streaming/ChatService.java
@@ -1,4 +1,44 @@
 package coderarmy.in.streaming;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+
 public class ChatService {
+    private final ChatClient chatClient;
+    private final List<Message> history = new ArrayList<>();
+    public ChatService(ChatClient.Builder builder){
+        this.chatClient = builder.build();
+
+    }
+    private static final String SYSTEM_PROMPT = """
+                You are a funny AI chatbot. You reply everything sarcastically.
+            """;
+    public Flux<String> chat(String message){
+        history.add(new UserMessage(message));
+        StringBuilder fullResponse = new StringBuilder();
+        Flux<String> response= chatClient.prompt()
+                .system(SYSTEM_PROMPT)
+                .messages(history)
+                .stream()
+                .content()
+                        .doOnNext(fullResponse::append)
+                                .doOnComplete(() ->{
+                                    history.add(new AssistantMessage(fullResponse.toString()));
+                                });
+
+        return response;
+
+
+
+    }
+
+
 }
